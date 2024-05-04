35f2c153de692345bb3ae05d9368f753e46ac8e0
japa <japatalent@gmail.com>
2024-05-04 09:54:55 +0100

    ii

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,34 +1,42 @@
-import Button from '@/common/Button/Button';
-import AppLayout from '@/components/layout/AppLayout';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import NewsImage from '@/assets/images/faq/news.webp';
-import QuestionList from '@/components/faq/QuestionList';
+import Button from "@/common/Button/Button";
+import AppLayout from "@/components/layout/AppLayout";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import NewsImage from "@/assets/images/faq/news.webp";
+import QuestionList from "@/components/faq/QuestionList";
 
 const FAQPage = () => {
   return (
     <AppLayout>
-      <div className='px-primary'>
-        <header className='faq-header-bg w-full h-[230px] items-center flex justify-center text-[30px] lg:text-[45px] text-center text-white rounded-[15px]'>
+      <div className="px-primary">
+        <header className="faq-header-bg w-full h-[230px] items-center flex justify-center text-[30px] lg:text-[45px] text-center text-white rounded-[15px]">
           <h1>FAQ</h1>
         </header>
-        <p className='text-lg lg:text-[22px] text-[#3D3D3D] mb-[52px] mt-[44px]'>
-          Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent
-          donec cras laoreet et penatibus. Mattis faucibus maecenas scelerisque purus nisl
-          rhoncus non ac. Amet erat viverra ut urna lectus nibh cursus tellus elit. Congue
-          tempor odio eu volutpat amet egestas. Iaculis amet natoque aliquet ligula est
-          orci vitae. Nulla nisl auctor nunc a aliquet hendrerit sed pellentesque auctor.
+        <p className="text-lg lg:text-[22px] text-[#3D3D3D] mb-[52px] mt-[44px]">
+          Welcome to Japa Faq! Here, we answer all the questions you have about
+          our tech-centric courses and offerings. At Japa, we pride ourselves on
+          providing a diverse array of courses tailored to suit every learner's
+          interests and career goals. While we currently specialize in code
+          tech, including courses in Artificial Intelligence (AI) and Machine
+          Learning, we're excited to announce our plans to expand our offerings
+          even further. Stay tuned for updates on our upcoming non-code tech
+          courses, designed to equip you with the skills and knowledge needed to
+          thrive in the rapidly evolving tech landscape.
         </p>
         <QuestionList />
-        <div className='pt-[89px] faq-bg'>
-          <h2 className='font-medium text-[25px] lg:text-[40px] mb-[84px]'>
+        <div className="pt-[89px] faq-bg">
+          <h2 className="font-medium text-[25px] lg:text-[40px] mb-[84px]">
             Stay up to date with JAPA news
           </h2>
-          <div className='relative'>
-            <Image src={NewsImage} alt='Blog' className='w-full h-full object-cover' />
-            <Link href='/blog' className='absolute bottom-9 right-[60px]'>
-              <Button className='!h-[76px] !w-[200px]'>Read our Blog</Button>
+          <div className="relative">
+            <Image
+              src={NewsImage}
+              alt="Blog"
+              className="w-full h-full object-cover"
+            />
+            <Link href="/blog" className="absolute bottom-9 right-[60px]">
+              <Button className="!h-[76px] !w-[200px]">Read our Blog</Button>
             </Link>
           </div>
         </div>
--- src/components/faq/questions.ts
+++ src/components/faq/questions.ts
@@ -5,28 +5,34 @@ interface Question {
 
 const questions: Question[] = [
   {
-    question:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras?',
+    question: "Does Japa Global help me relocate abroad?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras laoreet et penatibus. Mattis faucibus maecenas scelerisque purus nisl rhoncus non ac. Amet erat viverra ut urna lectus nibh cursus tellus elit. Congue tempor odio eu volutpat amet egestas. Iaculis amet natoque aliquet ligula est orci vitae. Nulla nisl auctor nunc a aliquet hendrerit sed pellentesque auctor.',
+      "We are a tech company focused on equipping talents with global in-demand skills to help them get hired faster",
+  },
+  {
+    question: "Do you have more courses than the one stated on the website?",
+    answer:
+      "We currently offer courses in code tech and plan  to include a wide variety of course options like Artificial Intelligence(AI), Machine Learning, etc. We are focused on non code tech and current do not offer courses that require coding like Web Development and Programming",
   },
   {
     question:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras?',
-    answer:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras laoreet et penatibus. Mattis faucibus maecenas scelerisque purus nisl rhoncus non ac. Amet erat viverra ut urna lectus nibh cursus tellus elit. Congue tempor odio eu volutpat amet egestas. Iaculis amet natoque aliquet ligula est orci vitae. Nulla nisl auctor nunc a aliquet hendrerit sed pellentesque auctor.',
+      "Do you help write Statement of Purpose (SOPS) if i plan to relocate?",
+    answer: "No we dont.",
   },
   {
     question:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras?',
-    answer:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras laoreet et penatibus. Mattis faucibus maecenas scelerisque purus nisl rhoncus non ac. Amet erat viverra ut urna lectus nibh cursus tellus elit. Congue tempor odio eu volutpat amet egestas. Iaculis amet natoque aliquet ligula est orci vitae. Nulla nisl auctor nunc a aliquet hendrerit sed pellentesque auctor.',
+      "Do you issue certificates after courses are taken on your platform?",
+    answer: "Yes, we offer a certificate of completion",
   },
   {
     question:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras?',
+      "Do you help write Statement of Purpose (SOPS) if i plan to relocate?",
+    answer: " No we dont.",
+  },
+  {
+    question: "Do you have more courses than the one stated on the website?",
     answer:
-      'Lorem ipsum dolor sit amet consectetur. Volutpat semper quis elementum praesent donec cras laoreet et penatibus. Mattis faucibus maecenas scelerisque purus nisl rhoncus non ac. Amet erat viverra ut urna lectus nibh cursus tellus elit. Congue tempor odio eu volutpat amet egestas. Iaculis amet natoque aliquet ligula est orci vitae. Nulla nisl auctor nunc a aliquet hendrerit sed pellentesque auctor.',
+      "We currently offer courses in code tech and plan  to include a wide variety of course options like Artificial Intelligence(AI), Machine Learning, etc. We are focused on non code tech and current do not offer courses that require coding like Web Development and Programming.",
   },
 ];
 
