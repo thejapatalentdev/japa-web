3d9182986050036b515a8d3b365fb5dc096547fb
japa <japatalent@gmail.com>
2024-05-05 16:37:36 +0100

    j

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -18,11 +18,11 @@ const FAQPage = () => {
           about our tech-centric courses and offerings. At Japa, we pride
           ourselves on providing a diverse array of courses tailored to suit
           every learner&apos;s interests and career goals. While we currently
-          specialize in code tech, including courses in Artificial Intelligence
-          (AI) and Machine Learning, we&apos;re excited to announce our plans to
-          expand our offerings even further. Stay tuned for updates on our
-          upcoming non-code tech courses, designed to equip you with the skills
-          and knowledge needed to thrive in the rapidly evolving tech landscape.
+          specialize In No Code Tech including Courses Data Analysis and
+          Product Management. We&apos;re excited to announce our plans to expand
+          our offerings even further. Stay tuned for updates on our upcoming
+          non-code tech courses, designed to equip you with the skills and
+          knowledge needed to thrive in the rapidly evolving tech landscape.
         </p>
         <QuestionList />
         <div className="pt-[89px] faq-bg">
@@ -35,8 +35,10 @@ const FAQPage = () => {
               alt="Blog"
               className="w-full h-full object-cover"
             />
-            <Link href="/blog" className="absolute bottom-9 right-[60px]">
-              <Button className="!h-[76px] !w-[200px]">Read our Blog</Button>
+            <Link href="/courses" className="absolute bottom-9 right-[60px]">
+              <Button className="!h-[76px] !w-[300px]">
+                Check Our Courses
+              </Button>
             </Link>
           </div>
         </div>
